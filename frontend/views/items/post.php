09200566c17e8da335f48aeb36d3c54b76d496e3
Markov Artur <36476213+artmarkov@users.noreply.github.com>
2019-04-16 17:55:36 +0300

    Update post.php

--- frontend/views/items/post.php
+++ frontend/views/items/post.php
@@ -12,7 +12,7 @@ $page = (isset($page)) ? $page : 'post';
     <h2><?= Html::a($post->title, ["/site/{$post->slug}"]) ?></h2>
 
     <p class="text-justify">
-        <?= $post->getThumbnail(['class' => 'thumbnail pull-left', 'style' => 'width: 160px; margin: 0 7px 7px 0']) ?>
+        <?//= $post->getThumbnail(['class' => 'thumbnail pull-left', 'style' => 'width: 160px; margin: 0 7px 7px 0']) ?>
         <?= ($page === 'post') ? $post->content : $post->shortContent ?>
     </p>
 
