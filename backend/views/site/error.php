a3175c99883a984a505e89cbdf7852ce35c5cf60
artmarkov <artmarkov@mail.ru>
2019-04-22 09:03:57 +0300

    Update error.php

--- backend/views/site/error.php
+++ backend/views/site/error.php
@@ -1,5 +1,4 @@
 <?php
-
 /* @var $this yii\web\View */
 /* @var $name string */
 /* @var $message string */
@@ -8,21 +7,30 @@
 use yii\helpers\Html;
 
 $this->title = $name;
+$this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-error">
+    <div class="container-fluid">
+        <div class="row">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+            <div class="col-md-12">
 
-    <div class="alert alert-danger">
-        <?= nl2br(Html::encode($message)) ?>
+                <div class="alert alert-danger alert-dismissible">
+                    <i class="fa fa-frown-o"></i> 
+                    <?= nl2br(Html::encode($message)) ?>
+                </div>
+
+                <div class="e404"><?= nl2br(Html::encode($code)) ?></div> 
+            <p>
+                The above error occurred while the Web server was processing your request.
+            </p>
+
+            <p>
+                Please contact us if you think this is a server error. Thank you.
+            </p>
+            </div>
+        </div>
     </div>
-
-    <p>
-        The above error occurred while the Web server was processing your request.
-    </p>
-
-    <p>
-        Please contact us if you think this is a server error. Thank you.
-    </p>
+</div>
 
 </div>
