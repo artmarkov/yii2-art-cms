47439648407ec0e6956373c6ce16fd69e7d851e3
artmarkov <artmarkov@mail.ru>
2019-04-30 19:01:13 +0300

    params

--- common/config/params.php
+++ common/config/params.php
@@ -1,7 +1,9 @@
 <?php
 return [
-    'adminEmail' => 'admin@example.com',
-    'supportEmail' => 'support@example.com',
+//    'adminEmail' => 'admin@example.com',
+//    'supportEmail' => 'support@example.com',
+    'adminEmail' => 'artmarkov@mail.ru',
+    'supportEmail' => 'artmarkov@mail.ru',
     'user.passwordResetTokenExpire' => 3600,
     'version' => '0.1.0',
 ];
